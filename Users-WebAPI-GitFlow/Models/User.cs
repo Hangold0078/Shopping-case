2bed536e80b0f88e7b21c2d5df3fc2a08b6a4b3a
Hangold0078 <eaa24hgo@students.eaaa.dk>
2026-08-27 12:46:37 +0200

    Added User model-class

---
+++ Users-WebAPI-GitFlow/Models/User.cs
@@ -0,0 +1,14 @@
+namespace Users_WebAPI_GitFlow.Models;
+
+public class User
+{
+    private int Id { get; set; }
+    private string Email { get; set; }
+    private string Password { get; set; }
+
+    public User(string email, string password)
+    {
+        this.Email = email;
+        this.Password = password;
+    }
+}
