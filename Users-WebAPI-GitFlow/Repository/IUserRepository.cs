a765acf7daa78b98c891b993fe3d5d29ce47af4e
EmiliePoulsen <eaa24eamp@students.eaaa.dk>
2026-08-31 13:21:58 +0200

    Added the new signatures

--- Users-WebAPI-GitFlow/Repository/IUserRepository.cs
+++ Users-WebAPI-GitFlow/Repository/IUserRepository.cs
@@ -5,5 +5,7 @@ namespace Users_WebAPI_GitFlow.Repository;
 public interface IUserRepository
 {
     public User Add(User user);
-    public User Find(User user);
+    public User GetUserByEmail(User user);
+    public User GetUserById(int id);
+    public List<User> GetAll();
 }
