74dc50f23a42fb8ff4331840741c611ceacf4dce
EmiliePoulsen <eaa24eamp@students.eaaa.dk>
2026-08-31 12:40:43 +0200

    added actionresult and made unauthorized and ok messages

--- Users-WebAPI-GitFlow/Controllers/UserController.cs
+++ Users-WebAPI-GitFlow/Controllers/UserController.cs
@@ -26,16 +26,16 @@ public class UserController : ControllerBase
 
    [HttpPost]
    [Route("login")]
-   public bool login(User user)
+   public ActionResult<User> login(User user)
    {
       User foundUser = _userRepository.Find(user);
       
          if (foundUser == null)
          {
-            return false;
+            return Unauthorized( new {message ="user not found"}); //kode 401
          }
 
-         return true;
+         return Ok(new {message = "Login successful", username = user.Email }); //kode 200
       
    }
 }
