ff69c82a5ef0f5c07a69df4ae6080a6491b62014
EmiliePoulsen <eaa24eamp@students.eaaa.dk>
2026-09-06 22:09:28 +0200

    tilføjede det login vi logger ind med og den user der sendes tilbage til testen

--- Users-WebAPI-GitFlow.Tests/UserRepositoryTests.cs
+++ Users-WebAPI-GitFlow.Tests/UserRepositoryTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Models;
 using Users_WebAPI_GitFlow.Controllers;
 using Users_WebAPI_GitFlow.Repository;
@@ -22,13 +23,17 @@ public class UserRepositoryTests
     public void LoginValidCredentials()
     {
         //Arrange
+        var login = new Login("Poul@mail.dk", "123"); //input, en bruger ville sende via login
+        var user = new User("Poul@mail.dk", "qwe", "salt"); //bruger, som repository'et skal returnere
+        
+        //Uanset hvad login-metoden kalder GetByEmail med, så lad som om vi fandt brugeren i databasen
         _mockRepository.Setup(r => r.GetByEmail(It.IsAny<Login>()))
-            .Returns(User);
+            .Returns(user);
         
         //Act
-        var result = _controller.Login("Poul@mail.dk");
+        var result = _controller.Login(login);
        
         //Assert
-        Assert.IsTrue(result);
+        Assert.IsInstanceOfType(result.Result, typeof(OkObjectResult));
     }
 }
